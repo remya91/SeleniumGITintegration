94d6753c740a883fd2cfb9f55277f5ef3ba21048
remya <remyaramaswamy1991@gmail.com>
2024-04-14 22:31:18 -0700

    Updated Demogit class by adding code for launching amazon website

--- Seleniumintegration/src/pack/Demogit.java
+++ Seleniumintegration/src/pack/Demogit.java
@@ -1,5 +1,17 @@
 package pack;
 
-public class Demogit {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class Demogit {
+	  @Test
+	  public void f() {
+		  System.setProperty("webdriver.chrome.driver","D:\\REMYA\\STC\\STC\\SELENIUM\\chrome-driver\\chromedriver-win64 (1)\\chromedriver-win64\\chromedriver.exe");
+		  WebDriver driver = new ChromeDriver();
+		  driver.get("https://www.amazon.in/");
+		  System.out.println("Amazon website launched");
+		  
+
+}
 }
